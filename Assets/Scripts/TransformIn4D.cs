61570b6c279627b4e2f32e3fc353c2fafa64b34c
Jeremie Landreville <jeremie.landreville@gmail.com>
2023-01-22 12:12:54 -0500

    bugfix for rotation + added water to the environment

--- Assets/Scripts/TransformIn4D.cs
+++ Assets/Scripts/TransformIn4D.cs
@@ -10,106 +10,82 @@ using System;
 public class TransformIn4D : MonoBehaviour
 {
     public Transform4 transform4;
-    public float verticalAxeRotation = 0.0f;
-    public float diagonalAxeRotation = 0.0f;
-    public float horizontalAxeRotation = 0.0f;
-    public float rotationSpeed = 10.0f;
-
-    Vector3 vec3;
+		Vector3 vec3;
     Vector3 newVec3;
-
-    Vector4 vecA4;
-    Vector4 vecB4;
-    Vector4 vecC4;
-    Vector4 vecD4;
-
-    Euler4 euler;
-
-    Matrix4 matrix;
-
+		Euler4 euler;
+    public float verticalAxeRotation = 5.0f;
+    public float diagonalAxeRotation = 5.0f;
+    public float horizontalAxeRotation = 5.0f;
+    public float rotationSpeed = 1f;
+ 
     private float myValue = 0f;
     private float click;
-		private bool rightMouseButtonClicked = false;
-		public bool lockCursor = true;
+		private bool lockCursor = true;
+
+		float lerpDuration = 5.0f;
+    bool rotating;
 
     void Update()
     {
 			horizontalAxeRotation = Input.GetAxis("Mouse X") * rotationSpeed;
-
-			verticalAxeRotation = Input.GetAxis("Mouse Y") * rotationSpeed;
+			verticalAxeRotation = Input.GetAxis("Mouse Y") * rotationSpeed;		
+			vec3 = new Vector3(diagonalAxeRotation, horizontalAxeRotation, verticalAxeRotation);
+			newVec3 = Vector3.Scale(vec3, new Vector3(2, 2, 2));
 
 			if (Input.GetKeyDown(KeyCode.Tab))
 			{
-				lockCursor = !lockCursor;
+					lockCursor = !lockCursor;
+					rotating = true;
 			}
  
 			if (lockCursor) {
 					Cursor.lockState = CursorLockMode.Locked;
-			} else {
+			}	else {
 					Cursor.lockState = CursorLockMode.None;
 			}
-			
+
 			if (Input.GetMouseButton(0))
 			{
-				RotateIn3D();
+				 transform.Rotate(0, horizontalAxeRotation, verticalAxeRotation);
+				 rotating = true;
 			}
 
 			if (Input.GetMouseButton(1))
 			{
-				RotateIn4D();
+					RotateIn4D(vec3, newVec3);
+					rotating = true;
+			}
+			if (!Input.anyKey && !rotating)
+			{
+				StartCoroutine(AutoRotate());
 			}
 
-			Debug.Log("Euler4 value: " + euler.ToString());
-			Debug.Log("MousePos value: " + Input.mousePosition);
-
-    }
-    void RotateIn3D()
-    {
-
-        Vector2 hub = new Vector2(Screen.width/2f, Screen.height/2f);
-
-        if (Input.GetMouseButton(1)) 
-        {
-					Vector2 mousePos = Input.mousePosition;
-					float mouseAngle = Vector2.Angle(Vector2.up, mousePos - hub);
-
-					if (mousePos.x<hub.x)
-					{
-						mouseAngle = 360f - mouseAngle;
-					}
-					if (Input.GetMouseButtonDown(0))
-					{
-						click = mouseAngle;
-					}
-					float difference = mouseAngle - click;
-					// To prevent error when going past zero
-					if (difference>180f)
-					{
-						difference-=360f;
-					}
-					if (difference<-180f)
-					{
-						difference+=360f;
-					}
-					myValue += difference * rotationSpeed;
-					click = mouseAngle;
-					myValue = Mathf.Clamp(myValue, -0f, 360f);
-        }
-
-				diagonalAxeRotation = myValue * rotationSpeed;
-
-        transform.Rotate(diagonalAxeRotation, horizontalAxeRotation, verticalAxeRotation);
-
     }
 
-    void RotateIn4D()
-    {
-			vec3 = new Vector3(diagonalAxeRotation, horizontalAxeRotation, verticalAxeRotation);
-			newVec3 = Vector3.Scale(vec3, new Vector3(2, 2, 2));
-
+		void RotateIn4D(Vector3 vec3, Vector3 newVec3)
+		{
 			euler = new Euler4(vec3, newVec3);
-
 			transform4.Rotate(euler, Space4.World);
-    }
+		}
+}
+
+/**
+		IEnumerator AutoRotate()
+		{
+			rotating = true;
+			float timeElapsed = 0;
+			Quaternion startRotation = transform.rotation;
+			Quaternion targetRotation = transform.rotation * Quaternion.Euler(0, 90, 0);
+			while (timeElapsed < lerpDuration)
+			{
+					transform.rotation = Quaternion.Slerp(startRotation, targetRotation, timeElapsed / lerpDuration);
+					timeElapsed += Time.deltaTime;
+					yield return new WaitForSeconds(2);;
+			}
+			transform.rotation = targetRotation;
+			/**
+
+		}
+		
 }
 
